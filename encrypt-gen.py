d02160a9034e87a1ac8a7432eb1c4e6b5038ebdf
jtpatriot <jtpatriot@me.com>
2013-01-11 11:28:11 -0800

    Cleaned up the code
    Removed global declarations, replaced with return functions. Simplified
    variable names within functions.

--- encrypt-gen.py
+++ encrypt-gen.py
@@ -17,8 +17,8 @@ def main():
 
 
 	def replacerDict():
-		global replacers
 		replacers = {' ':'', 'a':'@', 'e':'3', '':'', 'o':'0', 's':'5', 't':'7'}
+		return replacers
 
 
 	def welcomeMessage():
@@ -31,40 +31,39 @@ def main():
 		print """1) Generate a random character password."""
 		print """2) Randomize a specific word or phrase."""
 		print """3) Combination of 1 and 2."""
-		password_type_choice = raw_input("Enter a number selection: ")
+		choice = raw_input("Enter a number selection: ")
 		
-		password_type_loop = 1
-		while password_type_loop == 1:
+		loop = 1
+		while loop == 1:
 			try:
-				int(password_type_choice)
-				while int(password_type_choice) not in range(1,4):
-					password_type_choice = raw_input('Invalid number choice! \
+				int(choice)
+				while int(choice) not in range(1,4):
+					choice = raw_input('Invalid number choice! \
 Valid range is 1-3: ')
 				break
-				password_type_loop = 0
+				loop = 0
 			except ValueError:
-				password_type_choice = raw_input('You must specify a number! \
+				choice = raw_input('You must specify a number! \
 Valid range is 1-3: ')
 				
-		if password_type_choice == '1':
+		if choice == '1':
 			print ''
 			
-		elif password_type_choice == '2':
+		elif choice == '2':
 			print ''
 			
 			
-		elif password_type_choice == '3':
+		elif choice == '3':
 			print ''
 
-		return password_type_choice
+		return choice
 	
 
 
 	def stringInput():
-		global inputStringText
-		inputStringTextLoop = 1
 		print 'What is the word you want to be turned into a password?'
-		inputStringText = raw_input('Input word or phrase: ')
+		string = raw_input('Input word or phrase: ')
+		return string
 
 
 	def charReplace(text, dic):
@@ -78,30 +77,27 @@ Valid range is 1-3: ')
 	
 
 	def security_level():
-		global security_level_choice
 		print 'How many characters should the password have?'
 		print 'More characters means more secure. Suggested range is'
 		print 'between 10 and 30.'
-		security_level_choice = raw_input('Enter a number: ')
-		
-		security_level_loop = 1
-		while security_level_loop == 1:
+		choice = raw_input('Enter a number: ')
+		loop = 1
+		while loop == 1:
 			try:
-				int(security_level_choice)
-				while int(security_level_choice) not in range(1,10001):
+				int(choice)
+				while int(choice) not in range(1,10001):
 					print 'Invalid number choice! Valid range is 1-10,000.'
-					security_level_choice = raw_input('Enter a number: ')
-				security_level_choice = int(security_level_choice)
-				security_level_loop = 0
+					choice = raw_input('Enter a number: ')
+				choice = int(choice)
+				loop = 0
 			except ValueError:
 				print 'You must specify a number!'
-				security_level_choice = raw_input('Enter a number: ')
-		return security_level_choice
+				choice = raw_input('Enter a number: ')
+		return choice
 			
 
 	def security_chars():
 		# Character Strings
-		global pass_chars
 		pass_chars = '0'
 		hex_string_low = '0123456789abcdef'
 		alphabet_string_low = 'abcdefghijklmnopqrstuvwxyz'
@@ -115,7 +111,7 @@ Valid range is 1-3: ')
 		alph_low_true_str = 'Password will include lowercase alphabet. '
 		alph_up_true_str = 'Password will include uppercase alphabet. '
 		digit_true_str = 'Password will have digits. '
-		
+		# Selection
 		print 'What kind of characters would you like in your password?'
 		print '1) Hexadecimal. (0-9,a-f)'
 		print '2) Lowercase alphabet. (a-z)'
@@ -123,42 +119,42 @@ Valid range is 1-3: ')
 		print '4) Upper and lowercase alphabet. (A-Z,a-z)'
 		print '5) Lowercase alphabet with digits. (a-z,0-9)'
 		print '6) Digits. (0-9)'
-		security_chars_choice = raw_input('Enter selection (1-6): ')
-		
-		security_chars_loop = 1
-		while security_chars_loop == 1:
+		choice = raw_input('Enter selection (1-6): ')
+		# Loop
+		loop = 1
+		while loop == 1:
 			try:
-				int(security_chars_choice)
-				while int(security_chars_choice) not in range(1,7):
-					security_chars_choice = raw_input('Invalid number choice! \
+				int(choice)
+				while int(choice) not in range(1,7):
+					choice = raw_input('Invalid number choice! \
 Valid range is 1-6: ')
 				break
-				security_chars_loop = 0
+				loop = 0
 			except ValueError:
-				security_chars_choice = raw_input('You must specify a number! \
+				choice = raw_input('You must specify a number! \
 Valid range is 1-6: ')
 				
-		if security_chars_choice == '1':
+		if choice == '1':
 			pass_chars = hex_string_low
 			print hex_true_str
 			
-		elif security_chars_choice == '2':
+		elif choice == '2':
 			pass_chars = alphabet_string_low
 			print alph_low_true_str
 			
-		elif security_chars_choice == '3':
+		elif choice == '3':
 			pass_chars = alphabet_string_up
 			print alph_up_true_str
 			
-		elif security_chars_choice == '4':
+		elif choice == '4':
 			pass_chars = alphabet_string_low_up
 			print alph_low_true_str,alph_up_true_str
 			
-		elif security_chars_choice == '5':
+		elif choice == '5':
 			pass_chars = alphabet_string_low + digit_string
 			print alph_low_true_str,digit_true_str
 			
-		elif security_chars_choice == '6':
+		elif choice == '6':
 			pass_chars = digit_string
 			print digit_true_str
 
@@ -167,13 +163,10 @@ Valid range is 1-6: ')
 
 # Ask for symbols eventually
 
-	def generate_passphrase(pass_chars, security_level_choice):
-
-		list_of_chars = [random.choice(pass_chars) for n in xrange(security_level_choice)]
-		pass_string = "".join(list_of_chars)
-
-		print \
-'Passphrase with ', str(security_level_choice), 'characters: ', pass_string, ''
+	def generate_passphrase(characters, length):
+		listChars = [random.choice(characters) for n in xrange(length)]
+		result = "".join(listChars)
+		print 'Passphrase with ', str(length), 'characters: ', result, ''
 		
 
 
@@ -187,15 +180,15 @@ Valid range is 1-6: ')
 
 		passwordType_selection = passwordType()
 		if passwordType_selection in ('1'):
-			security_chars()
+			pass_chars = security_chars()
 			printBlankLine(1)
-			security_level()
+			security_level_choice = security_level()
 			printBlankLine(1)
 			generate_passphrase(pass_chars, security_level_choice)
 			printBlankLine(1)
 		elif passwordType_selection in ('2'):
-			stringInput()
-			replacerDict()
+			inputStringText = stringInput()
+			replacers = replacerDict()
 			results = charReplace(inputStringText, replacers)
 			printBlankLine(1)
 			printResults(results)
