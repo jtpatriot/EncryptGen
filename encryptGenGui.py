afb4a34336f94350d63471bbbb6b484bda06e9be
jtpatriot <jtpatriot@me.com>
2013-01-16 23:48:39 -0800

    Broken but progress

--- encryptGenGui.py
+++ encryptGenGui.py
@@ -1,92 +1,61 @@
 # Password Generator
 # 
 # 
-# 
-#
-#
 import random
 from Tkinter import *
 import ttk
 
 
-def printBlankLine(amount):
-	while amount >> 0:
-		print ''
-		amount = amount - 1
-
-
 def replacerDict():
-	replacers = {' ':'', 'a':'@', 'e':'3', 'o':'0', 's':'5', 't':'7'}
-	return replacers
+    replacers = {' ':'', 'a':'@', 'e':'3', 'o':'0', 's':'5', 't':'7'}
+    return replacers
 
 
 def stringInput():
-	print 'What is the word you want to be turned into a password?'
-	string = raw_input('Input word or phrase: ')
-	return string
+    print ''
 
 
 def charReplace(text, dic):
-	for i, j in dic.iteritems():
-		text = text.replace(i, j)
-	return text
-
-
-def printResults(results):
-	print 'The secured string is:', results
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+        return text
 
 
 def security_level(number):
     try:
-        int(number)
-	while int(number) not in range(1,10001):
-	    number = raw_input('Enter a number: ')
-	    number = int(number)
-	    loop = 0
-    except ValueError:
-	number = raw_input('Enter a number: ')
-    return number
-		
+        number = int(number)
+        endNumber.set(number)
+    except:
+        printStr.set('Please enter a number above.')
+    return endNumber
+        
 
-def security_chars(number):
-    # Character Strings
-    pass_chars = '0'
-    hex_string_low = '0123456789abcdef'
-    alphabet_string_low = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_string_up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    alphabet_string_low_up = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    digit_string = '0123456789'
-    symbol_string = """!@#$%^&*()"""
-    at_pound_string = """@#"""
-    
-    if number == 1:
-        pass_chars = hex_string_low
-        print hex_true_str
-                    
-    elif number == 2:
-        pass_chars = alphabet_string_low
-        print alph_low_true_str
-                    
-    elif number == 3:
-        pass_chars = alphabet_string_up
-        print alph_up_true_str
-
+def containStrings():
+    pass_chars = StringVar('')
+    if(check1.get() == 1):
+        print "check1 is on"
+        pass_chars.set('1234567890')
+    if(check2.get() == 1):
+        print "check2 is on"
+        pass_chars.set('abcdefghijklmnopqrstuvwxyz')
+    if(check3.get() == 1):
+        print "check3 is on"
+        pass_chars.set('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     return pass_chars
 
 
-# Ask for symbols eventually...
-
-def generate_passphrase(characters, length):
-	listChars = [random.choice(characters) for n in xrange(length)]
-	result = "".join(listChars)
-	print 'Passphrase with ', str(length), 'characters: ', result, ''
-
+def generate():    
+    passChars.set(containStrings())
+    length.set(security_level(numCount))
+    listChars = [random.choice(passChars) for n in xrange(length)]
+    result = "".join(listChars)
+    printStr.set(result)
 
 
 root = Tk()
 root.title("EncryptGen")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12", width=600, height=300)
+mainframe = ttk.Frame(root, padding="3 3 12 12", width=500, height=300)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(1, weight=1)
 mainframe.rowconfigure(1)
@@ -97,9 +66,20 @@ mainframe.rowconfigure(4)
 mainframe.rowconfigure(5)
 mainframe.grid_propagate(0)
 
-password = StringVar()
-printStr = StringVar()
-description = StringVar()
+numCount = StringVar('')
+printStr = StringVar('')
+description = StringVar('')
+
+check1 = IntVar(0)
+check2 = IntVar(0)
+check3 = IntVar(0)
+
+length = StringVar('')
+passChars = StringVar('')
+listChars = []
+result = StringVar('')
+
+endNumber = IntVar()
 
 description.set("""Modes:\n"""\
 """1) Generate a random character password.\n"""\
@@ -108,8 +88,8 @@ description.set("""Modes:\n"""\
 
 printStr.set('')
 
-password_entry = ttk.Entry(mainframe, width=7, textvariable=password)
-password_entry.grid(column=2, row=3, sticky=(W, E, N))
+numCount_entry = ttk.Entry(mainframe, width=20, textvariable=numCount)
+numCount_entry.grid(column=2, row=3, sticky=(W, E, N))
 
 ttk.Entry(mainframe, textvariable=printStr).grid(column=2, row=5, sticky=(W, E, N))
 
@@ -117,19 +97,19 @@ ttk.Button(mainframe, text="Mode 1", command='').grid(column=2, row=1, sticky=(W
 ttk.Button(mainframe, text="Mode 2", command='').grid(column=2, row=1, sticky=(N))
 ttk.Button(mainframe, text="Mode 3", command='').grid(column=2, row=1, sticky=(E, N))
 
-ttk.Checkbutton(mainframe, text="numbers", command=security_chars(1)).grid(column=2, row=4, sticky=(W, N))
-ttk.Checkbutton(mainframe, text="lowercase", command=security_chars(2)).grid(column=2, row=4, sticky=(N))
-ttk.Checkbutton(mainframe, text="uppercase", command=security_chars(3)).grid(column=2, row=4, sticky=(E, N))
+ttk.Checkbutton(mainframe, text="Numbers", variable=check1).grid(column=2, row=4, sticky=(W, N))
+ttk.Checkbutton(mainframe, text="Lowercase", variable=check2).grid(column=2, row=4, sticky=(N))
+ttk.Checkbutton(mainframe, text="Uppercase", variable=check3).grid(column=2, row=4, sticky=(E, N))
 
 
 ttk.Label(mainframe, textvariable=description).grid(column=2, row=2, sticky=NW)
 ttk.Label(mainframe, text="Number of\nCharacters:").grid(column=1, row=3, sticky=NW)
-ttk.Button(mainframe, text="Generate").grid(column=1, row=5, sticky=NW)
+ttk.Button(mainframe, text="Generate", command=generate).grid(column=1, row=5, sticky=NW)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
-password_entry.focus()
-root.bind('<Return>', '')
+numCount_entry.focus()
+root.bind('<Return>', generate)
 
 root.mainloop()
 
