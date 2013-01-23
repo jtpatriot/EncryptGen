a49c4ca988c538673a0a995d89ba89fea075eaca
jtpatriot <jtpatriot@me.com>
2013-01-22 20:46:05 -0800

    Changes to displaying Mode content

--- encryptGenGui.py
+++ encryptGenGui.py
@@ -6,19 +6,44 @@ from Tkinter import *
 import ttk
 
 
-def displayMode(number):
-    if number == 1:
-        mode2frame.grid_remove()
-        mode3frame.grid_remove()
-        mode1frame.grid()
-    if number == 2:
-        mode3frame.grid_remove()
-        mode1frame.grid_remove()
-        mode2frame.grid(column=1, row=3, sticky=(N, W, E, S))
-    if number == 3:
-        mode2frame.grid_remove()
-        mode1frame.grid_remove()
-        mode3frame.grid(column=1, row=3, sticky=(N, W, E, S))
+def displayMode1():
+    removeMode2()
+    removeMode3()
+    numCount_entry.grid(column=2, row=1, sticky=(W, E, N))
+    numCount_label.grid(column=1, row=1, sticky=NW)
+    mode1Check1.grid(column=2, row=2, sticky=(W, N))
+    mode1Check2.grid(column=2, row=2, sticky=(N))
+    mode1Check3.grid(column=2, row=2, sticky=(E, N))
+    mode1Generate.grid(column=1, row=3, sticky=NW)
+    mode1Results.grid(column=2, row=3, sticky=(W, E, N))
+    
+
+def displayMode2():
+    removeMode1()
+    removeMode3()
+    
+
+def displayMode3():
+    removeMode1()
+    removeMode2()
+    
+
+def removeMode1():
+    numCount_entry.grid_remove()
+    numCount_label.grid_remove()
+    mode1Check1.grid_remove()
+    mode1Check2.grid_remove()
+    mode1Check3.grid_remove()
+    mode1Generate.grid_remove()
+    mode1Results.grid_remove()
+
+
+def removeMode2():
+    print ''
+
+
+def removeMode3():
+    print ''
 
 
 def replacerDict():
@@ -75,6 +100,7 @@ def generate():
 root = Tk()
 root.title("EncryptGen")
 
+
 mainframe = ttk.Frame(root, padding="3 3 12 12", width=500, height=300)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(1)
@@ -83,13 +109,13 @@ mainframe.rowconfigure(2)
 mainframe.rowconfigure(3)
 mainframe.grid_propagate(0)
 
-mode1frame = ttk.Frame(mainframe, padding="3 3 12 12", width=480, height=200)
+modeframe = ttk.Frame(mainframe, padding="3 3 12 12", width=480, height=200)
 #mode2frame = ttk.Frame(mainframe, padding="3 3 12 12", width=480, height=200)
 #mode3frame = ttk.Frame(mainframe, padding="3 3 12 12", width=480, height=200)
 
-mode1frame.grid(column=1, row=3, sticky=(W, E, S))
+modeframe.grid(column=1, row=3, sticky=(W, E, S))
 
-mode1frame.grid_propagate(0)
+modeframe.grid_propagate(0)
 
 numCount = StringVar('')
 printStr = StringVar('')
@@ -98,6 +124,7 @@ description = StringVar('')
 check1 = IntVar(0)
 check2 = IntVar(0)
 check3 = IntVar(0)
+check4 = IntVar(0)
 
 length = IntVar()
 passChars = StringVar('')
@@ -112,36 +139,31 @@ description.set("""Modes:\n"""\
 printStr.set('')
 
 
-
-mode1Results = ttk.Entry(mode1frame, textvariable=printStr)
-mode1Results.grid(column=2, row=3, sticky=(W, E, N))
-
-mode1Button = ttk.Button(mainframe, text="Mode 1", command='')
+mode1Button = ttk.Button(mainframe, text="Mode 1", command=displayMode1)
 mode1Button.grid(column=1, row=1, sticky=(W, N))
-mode2Button = ttk.Button(mainframe, text="Mode 2", command='')
+mode2Button = ttk.Button(mainframe, text="Mode 2", command=displayMode2)
 mode2Button.grid(column=1, row=1, sticky=(N))
-mode3Button = ttk.Button(mainframe, text="Mode 3", command='')
+mode3Button = ttk.Button(mainframe, text="Mode 3", command=displayMode3)
 mode3Button.grid(column=1, row=1, sticky=(E, N))
 
 descriptionLabel = ttk.Label(mainframe, textvariable=description)
 descriptionLabel.grid(column=1, row=2, sticky=N)
 
-numCount_entry = ttk.Entry(mode1frame, width=42, textvariable=numCount)
-numCount_entry.grid(column=2, row=1, sticky=(W, E, N))
-numCount_label = ttk.Label(mode1frame, text="Number of\nCharacters:")
-numCount_label.grid(column=1, row=1, sticky=NW)
+
+### Mode 1 objects         ###
+numCount_entry = ttk.Entry(modeframe, width=42, textvariable=numCount)
+numCount_label = ttk.Label(modeframe, text="Number of\nCharacters:")
+
+mode1Check1 = ttk.Checkbutton(modeframe, text="Numbers", variable=check1)
+mode1Check2 = ttk.Checkbutton(modeframe, text="Lowercase", variable=check2)
+mode1Check3 = ttk.Checkbutton(modeframe, text="Uppercase", variable=check3)
+
+mode1Generate = ttk.Button(modeframe, text="Generate", command=generate)
+mode1Results = ttk.Entry(modeframe, textvariable=printStr)
+###############################
 
 
-mode1Check1 = ttk.Checkbutton(mode1frame, text="Numbers", variable=check1)
-mode1Check1.grid(column=2, row=2, sticky=(W, N))
-mode1Check2 = ttk.Checkbutton(mode1frame, text="Lowercase", variable=check2)
-mode1Check2.grid(column=2, row=2, sticky=(N))
-mode1Check3 = ttk.Checkbutton(mode1frame, text="Uppercase", variable=check3)
-mode1Check3.grid(column=2, row=2, sticky=(E, N))
 
-
-mode1Generate = ttk.Button(mode1frame, text="Generate", command=generate)
-mode1Generate.grid(column=1, row=3, sticky=NW)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
