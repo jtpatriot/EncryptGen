ff087209990acc6c08651f759fc47fc26da2c02d
jtpatriot <jtpatriot@me.com>
2013-01-23 10:39:28 -0800

    Working on modes 2 and 3

--- encryptGenGui.py
+++ encryptGenGui.py
@@ -6,6 +6,7 @@ from Tkinter import *
 import ttk
 
 
+### Set the Displayed Widgets
 def displayMode1():
     removeMode2()
     removeMode3()
@@ -17,21 +18,24 @@ def displayMode1():
     mode1Generate.grid(column=1, row=3, sticky=NW)
     mode1Results.grid(column=2, row=3, sticky=(W, E, N))
     numCount_entry.focus()
-    
-
 def displayMode2():
     removeMode1()
     removeMode3()
-    textToMix_entry.grid(column=2, row=1, sticky=(W, E, N))
-    mode2Result_box.grid(column=2, row=2, sticky=(W, N))
+    textToMix_label.grid(column=1, row=1, sticky=(W, N))
     mode2Generate.grid(column=1, row=2, sticky=(W, N))
-    
-
+    textToMix_entry.grid(column=2, row=1, sticky=(W, E))
+    mode2Result_box.grid(column=2, row=2, sticky=(W, E, N))
 def displayMode3():
     removeMode1()
     removeMode2()
+    mode3Input1_label.grid(column=1, row=1, sticky=(W, N))
+    mode3Generate.grid(column=1, row=2, sticky=(W, N))
+    mode3Input1_entry.grid(column=2, row=1, sticky=(W, E))
+    mode3Result_box.grid(column=2, row=2, sticky=(W, E, N))
+####################
     
 
+### Remove Deselected Widgets
 def removeMode1():
     numCount_entry.grid_remove()
     numCount_label.grid_remove()
@@ -40,31 +44,20 @@ def removeMode1():
     mode1Check3.grid_remove()
     mode1Generate.grid_remove()
     mode1Results.grid_remove()
-
-
 def removeMode2():
-    good = 'good'
-
-
+    textToMix_label.grid_remove()
+    mode2Generate.grid_remove()
+    textToMix_entry.grid_remove()
+    mode2Result_box.grid_remove()
 def removeMode3():
-    bad = 'bad'
+    mode3Input1_label.grid_remove()
+    mode3Generate.grid_remove()
+    mode3Input1_entry.grid_remove()
+    mode3Result_box.grid_remove()
+####################
 
 
-def replacerDict():
-    replacers = {' ':'', 'a':'@', 'e':'3', 'o':'0', 's':'5', 't':'7'}
-    return replacers
-
-
-def stringInput():
-    print ''
-
-
-def charReplace(text, dic):
-    for i, j in dic.iteritems():
-        text = text.replace(i, j)
-        return text
-
-
+### Mode 1 Functions
 def security_level(number):
     try:
         number = int(number)
@@ -72,8 +65,6 @@ def security_level(number):
     except:
         printStr.set('Please enter a number above.')
     return endNumber.get()
-        
-
 def containStrings(check1, check2, check3):
     pass_chars = StringVar('')
     if(check1 == 1):
@@ -83,8 +74,6 @@ def containStrings(check1, check2, check3):
     if(check3 == 1):
         pass_chars.set(pass_chars.get() + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     return pass_chars.get()
-
-
 def randomizeJoin(x, y):
     try:
         listed = [random.choice(x) for n in xrange(y)]
@@ -92,23 +81,30 @@ def randomizeJoin(x, y):
         return results
     except IndexError:
         return 'Error: Please make a checkbox selection.'
+####################
 
 
-def mode2combiner(x, y):
-    List = [x,y]
-    return "".join(List)
+### Mode 2 Functions
+def replacerDict():
+    replacers = {' ':'', 'a':'@', 'e':'3', 'o':'0', 's':'5', 't':'7'}
+    return replacers
+def charReplace(text, dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
+####################
 
 
+### Generate Results Functions
 def generateMode1():    
     passChars.set(containStrings(check1.get(), check2.get(), check3.get()))
     length.set(security_level(numCount.get()))
     printStr.set(randomizeJoin(passChars.get(), length.get()))
-
-    
 def generateMode2():    
-    hello = 'hello '
-    world = 'world'
-    mode2results.set(mode2combiner(hello, world))
+    mode2results.set(charReplace(str(textToMix.get()), replacerDict()))
+def generateMode3():
+    mode3results.set('Mode 3 is not functional yet.')
+####################
 
 
 root = Tk()
@@ -153,10 +149,14 @@ passChars = StringVar('')
 endNumber = IntVar()
 ####################
 
-
 ### Mode 2 variables
 textToMix = StringVar('')
 mode2results = StringVar('')
+####################
+
+### Mode 3 variables
+textInputMode3 = StringVar('')
+mode3results = StringVar('')
 ####################
 
 
@@ -170,11 +170,18 @@ mode1Generate = ttk.Button(modeframe, text="Generate", command=generateMode1)
 mode1Results = ttk.Entry(modeframe, textvariable=printStr)
 ##################
 
-
 ### Mode 2 objects
 textToMix_entry = ttk.Entry(modeframe, width=42, textvariable=textToMix)
+textToMix_label = ttk.Label(modeframe, text="Word or Phrase\nto scramble:")
 mode2Result_box = ttk.Entry(modeframe, textvariable=mode2results)
-mode2Generate = ttk.Button(modeframe, text="Generate", command=generateMode2)
+mode2Generate = ttk.Button(modeframe, text="Scramble", command=generateMode2)
+##################
+
+### Mode 3 objects
+mode3Input1_entry = ttk.Entry(modeframe, width=42, textvariable=textInputMode3)
+mode3Input1_label = ttk.Label(modeframe, text="Word or Phrase\nto scramble:")
+mode3Result_box = ttk.Entry(modeframe, textvariable=mode3results)
+mode3Generate = ttk.Button(modeframe, text="Generate", command=generateMode3)
 ##################
 
 
